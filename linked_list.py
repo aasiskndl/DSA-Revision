4ffd3fb9a15e226a688c37568acad56f2f9b9c07
aasiskndl <aashishkandel90@gmail.com>
2026-01-11 22:34:41 +0545

    Learnt about the linked list

---
+++ linked_list.py
@@ -0,0 +1,6 @@
+# linked list is a linear data structure where elements are not stored in contigious memory
+# each element is called a node and every node contains [data|next] values
+# linked list are dynamic in size and they are efficient in performing operations for insertion and deletion 
+
+# there are different types of linked list but toda, we are going to learn about simple linked list
+
