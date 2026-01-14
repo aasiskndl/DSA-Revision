4f8a7ba41d2448661f07984d998675155046e902
aasiskndl <aashishkandel90@gmail.com>
2026-01-14 23:03:20 +0545

    Learnt about the binary search tree

---
+++ binary_search_tree.py
@@ -0,0 +1,6 @@
+''' binary search tree is a special kind of binary tree where: 
+1. left subtree contains nodes with value less than that of the root 
+2. right subtree contains the nodes with values greater than the root 
+3. if we divide them to individual trees i.e. left subtree and right subtree ten they are also binary trees individually 
+'''
+
