268f1281e8b993be4af21ad5576caa850436c5ef
aasiskndl <aashishkandel90@gmail.com>
2026-01-14 23:14:50 +0545

    Added a simple example of binary search tree to search the items that are in the graph/nodes

--- binary_search_tree.py
+++ binary_search_tree.py
@@ -4,3 +4,51 @@
 3. if we divide them to individual trees i.e. left subtree and right subtree ten they are also binary trees individually 
 '''
 
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+        
+def insert(root, key):
+    if root is None:
+        return Node(key)
+    
+    if key < root.data:
+        root.left = insert(root.left, key)
+    elif key > root.data:
+        root.right = insert(root.right, key)
+    return root
+
+def search(root, key):
+    if root is None or root.data == key:
+        return root
+    if key < root.data:
+        return search(root.left, key)
+    else:
+        return search(root.right, key)
+    
+def inorder(root):
+    if root:
+        inorder(root.left)
+        print(root.data, end=" ")
+        inorder(root.right)
+        
+# example usage
+root = None
+values = [50, 30, 70, 20, 40, 60, 80]
+
+for val in values:
+    root = insert(root, val)
+    
+print("Inorder traversal of BST:")
+inorder(root)
+
+#search for a key
+key = 19
+found = search(root, key)
+if found:
+    print(f"\n{key} found in BST")
+    
+else:
+    print(f"\n{key} not found in BST")
