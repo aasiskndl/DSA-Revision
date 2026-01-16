02613d5d3df243289eb3ffd092c0e8da37ea5862
aasiskndl <aashishkandel90@gmail.com>
2026-01-16 22:06:42 +0545

    Learnt about the concept of heap

---
+++ heap.py
@@ -0,0 +1,5 @@
+# a heap is a complete binary tree that follows the heap property
+# min heap: parent node <= its children (smallest element is always at the root)
+# max heap: parent node >= its children  (largest element is always at the root)
+
+
