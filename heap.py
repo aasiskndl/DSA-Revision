4664106907979940fa28a1de356aaa5536d261bf
aasiskndl <aashishkandel90@gmail.com>
2026-01-16 22:12:27 +0545

    Implemented an example of heap

--- heap.py
+++ heap.py
@@ -3,3 +3,21 @@
 # max heap: parent node >= its children  (largest element is always at the root)
 
 
+import heapq
+
+heap = []
+
+# insert the elements
+heapq.heappush(heap, 20)
+heapq.heappush(heap, 10)
+heapq.heappush(heap, 30)
+heapq.heappush(heap, 5)
+
+print("Heap values: ", heap)
+
+#peek (min element)
+print("Min element: ", heap[0])
+
+# delete (remove min)
+print("Deleted element: ", heapq.heappop(heap))
+print("Heap after deletion: ", heap)
