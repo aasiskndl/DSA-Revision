c024268f34310844427624df07eed92ca6dff265
aasiskndl <aashishkandel90@gmail.com>
2026-01-17 22:53:04 +0545

    Learnt about the graph and its properties

---
+++ graph.py
@@ -0,0 +1,9 @@
+# graph is a non-linear data structure consisting of finite set of vertices (nodes) and a set of edges that connect pairs of vertices.
+# a graph can be represented as: G = (V, E)
+# where V is the set fo vertices (nodes)
+# E is the set of edges (connections betwn vertices)
+
+# graphs can be used to model real world relationships (network, maps, social media)
+# a graph does not have a single root
+# nodes can have any number of connections
+# cycles may or may not exist
