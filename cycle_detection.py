c1826e0eabe166b901416480ba3bfeb920fc80c2
aasiskndl <aashishkandel90@gmail.com>
2026-01-18 15:22:35 +0545

    Learnt about the cycle detection in graphj

---
+++ cycle_detection.py
@@ -0,0 +1,38 @@
+# a cycle in graph is a path in which a vertex is reached again by following edges, without repeating edges, and the path lenght is greater than zero.
+
+#example representation 
+
+def has_cycle(graph):
+    visited = set ()
+    
+    def dfs(node, parent):
+        visited.add(node)
+        
+        for neighbour in graph[node]:
+            if neighbour not in visited:
+                if dfs(neighbour, node):
+                    return True
+                
+            elif neighbour != parent:
+                return True
+            
+        return False
+    
+    for node in graph:
+        if node not in visited:
+            if dfs(node, -1):
+                return True
+    return False
+
+graph = {
+    0: [1,2], 
+    1: [0,2],
+    2: [0,1],
+    3: []
+}
+
+if has_cycle(graph):
+    print("Graph has a cycle")
+    
+else:
+    print("Graph has no cycle")
