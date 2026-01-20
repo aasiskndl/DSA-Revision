37f05c456f7241d25cad562a4d8afc0ff2f1c642
aasiskndl <aashishkandel90@gmail.com>
2026-01-20 17:14:05 +0545

    Learnt the implementation of union find

--- union_find.py
+++ union_find.py
@@ -12,4 +12,36 @@ two main operations:
 - union(x,y): merges the set containing x and y
 '''
 
+class UnionFind:
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.rank = [0] * n
+        
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+    
+    def union(self, x, y):
+        root_x = self.find(x)
+        root_y = self.find(y)
+        
+        if root_x == root_y:
+            return
+        
+        if self.rank[root_x] < self.rank[root_y]:
+            self.parent[root_x] = root_y
+        elif self.rank[root_x] > self.rank[root_y]:
+            self.parent[root_y] = root_x
+        else:
+            self.parent[root_y] = root_x
+            self.rank[root_x] += 1
+            
+uf = UnionFind(5)
+uf.union(0,1)
+uf.union(1,2)
+uf.union(3,4)
+
+print(uf.find(0) == uf.find(2))
+print(uf.find(0) == uf.find(4))
 
