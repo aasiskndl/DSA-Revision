1b172f4a304a8a69c6202cdf6ed6099b33e29da6
aasiskndl <aashishkandel90@gmail.com>
2026-01-21 17:42:32 +0545

    Implemented the kruskals algo example

--- kruskals_algo.py
+++ kruskals_algo.py
@@ -22,3 +22,57 @@ steps:
 4. stop when mst has V - 1 edges
 
 '''
+
+class UnionFind:
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.rank = [0] * n 
+        
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+    
+    def union(self, x, y):
+        root_x = self.find(x)
+        root_y = self.find(y)
+        
+        if root_x == root_y:
+            return False
+        
+        if self.rank[root_x] < self.rank[root_y]:
+            self.parent[root_x] = root_y
+        elif self.rank[root_x]> self.rank[root_y]:
+            self.parent[root_y] = root_x
+        else:
+            self.parent[root_y] = root_x
+            self.rank[root_x] +=1
+            
+        return True
+
+def kruskal(n, edges):
+    edges.sort(key=lambda x: x[2])
+    uf = UnionFind(n)
+    
+    mst = []
+    total_weight = 0
+    
+    for u,v,w in edges:
+        if uf.union(u, v):
+            mst.append((u, v, w))
+            total_weight += w
+            
+    return mst, total_weight
+
+edges = [
+    (0, 1, 1),
+    (1, 2, 2),
+    (0, 2, 3),
+    (1, 3, 4),
+    (2, 3, 5)
+]
+
+mst, weight = kruskal(4, edges)
+
+print("MST edges: ", mst)
+print("Total weights: ", weight)
