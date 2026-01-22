a0ed0234b0aa637147bc2a0c5e060325a005df0c
aasiskndl <aashishkandel90@gmail.com>
2026-01-22 12:28:03 +0545

    Learnt about dijkstra algorithm

---
+++ dijkstra.py
@@ -0,0 +1,10 @@
+'''
+Docstring for dijkstra
+Dijstra's algorithm is a greedy algorith that finds the shortest path from a given source node to all other nodes in a weighted graph 
+with non-negative edges
+
+- weighted graph
+- no negative weights
+- single source -> all destinations
+'''
+
