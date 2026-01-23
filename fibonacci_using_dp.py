56531838eb8b1585ffadb3dd9e290ea8e6ac248b
aasiskndl <aashishkandel90@gmail.com>
2026-01-23 22:56:17 +0545

    Added example of dynamic programming

--- fibonacci_using_dp.py
+++ fibonacci_using_dp.py
@@ -7,5 +7,14 @@ we can do it by:
 
 '''
 
+def fib(n):
+    dp = [0] * (n+1)
+    dp[1] = 1
+    
+    for i in range (2, n + 1):
+        dp[i] = dp[i-1] + dp[i-2]
+        
+    return dp[n]
 
+print(fib(5))
 
