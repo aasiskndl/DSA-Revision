33fafa304f494dd0c76a14950e22d1fd80aefe9d
aasiskndl <aashishkandel90@gmail.com>
2026-01-23 22:30:33 +0545

    learnt about the dynamic programming concept

---
+++ fibonacci_using_dp.py
@@ -0,0 +1,11 @@
+'''
+Dynamic programming is the technique that solves a big problem by solving small problems one and then reusing them to solve a big problem
+
+we can do it by:
+- overlapping subproblems
+- store results
+
+'''
+
+
+
