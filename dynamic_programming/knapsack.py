dcb8b503f926f0138f2718a51a32c146379d5908
aasiskndl <aashishkandel90@gmail.com>
2026-01-25 17:55:37 +0545

    Learnt about the knapsack problem

---
+++ dynamic_programming/knapsack.py
@@ -0,0 +1,12 @@
+'''
+Docstring for dynamic_programming.knapsack
+You are given n items, each has its respective weights, values.
+You have a knapsack that can carry capacity of W
+
+Rule:
+- you can either take an item or not to take it 
+- you cannot take the same item twice (that's why it is called 0/1)
+
+Goal: is to maximize the total value without exceeding capacity.
+'''
+
