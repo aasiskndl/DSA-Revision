bef08ad51807f00b8cbe2655eaeb52c1701234d9
aasiskndl <aashishkandel90@gmail.com>
2026-01-26 21:20:58 +0545

    Learnt about the subset sum problem

---
+++ dynamic_programming/subset_sum_problem.py
@@ -0,0 +1,8 @@
+'''
+Docstring for dynamic_programming.subset_sum_problem
+You are given a list of positive integers (numbers) and a target value (target)
+
+goal is to determine if there exists a subset of numbers whose sum equals to the target value
+we only return true or false
+'''
+
