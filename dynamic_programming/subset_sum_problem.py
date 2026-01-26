872e745123ffe7eb79070c5981ebd79912d5652e
aasiskndl <aashishkandel90@gmail.com>
2026-01-26 21:33:28 +0545

    Implemented the example of subset_sum problem

--- dynamic_programming/subset_sum_problem.py
+++ dynamic_programming/subset_sum_problem.py
@@ -6,3 +6,23 @@ goal is to determine if there exists a subset of numbers whose sum equals to the
 we only return true or false
 '''
 
+def subset_sum(nums, target):
+    n = len(nums)
+    dp = [[False] * (target + 1) for _ in range(n + 1)]
+    
+    # base case: sum 0 is always possible
+    for i in range(n+1):
+        dp[i][0] = True
+        
+    for i in range(1, n+1):
+        for s in range(1, target+1):
+            if nums[i-1]>s:
+                dp[i][s] = dp[i-1][s]
+            else:
+                dp[i][s] = dp[i-1][s] or dp[i-1][s-nums[i-1]]
+                
+    return dp[n][target]
+    
+nums = [3,34,4,12,5,2]
+target = 9
+print(subset_sum(nums, target))
