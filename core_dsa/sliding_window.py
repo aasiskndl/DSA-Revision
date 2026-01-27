9670e7c5c15f67abb9925378c1b06d0eca62d2b0
aasiskndl <aashishkandel90@gmail.com>
2026-01-27 21:42:34 +0545

    Learnt about the sliding window problem

---
+++ core_dsa/sliding_window.py
@@ -0,0 +1,14 @@
+'''
+Docstring for core_dsa.sliding_window
+Sliding window is the type of problem in which you are given a sequence of arrays/strings and the goal is to find the longest
+subset or patterns within contiguous data.
+It uses two pointers (usually left and right) to define the boundar of the active window.
+
+two types:
+1. fixed size
+2. variable size (dynamic)
+
+It reduces time complexit from O(n^2) to O(n) by reusing results from the previous window calculation 
+
+'''
+
