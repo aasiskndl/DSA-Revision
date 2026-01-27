602086828682f1e271ebf48d12f0818ab3e17199
aasiskndl <aashishkandel90@gmail.com>
2026-01-27 21:50:16 +0545

    Implemented the example of sliding window problem

--- core_dsa/sliding_window.py
+++ core_dsa/sliding_window.py
@@ -12,3 +12,27 @@ It reduces time complexit from O(n^2) to O(n) by reusing results from the previo
 
 '''
 
+def len_of_longest_substring(s):
+    char_set = set()
+    left = 0 
+    max_length = 0
+    
+    for right in range(len(s)):
+        # if duplicate character appears then srink window from left
+        while s[right] in char_set:
+            char_set.remove(s[left])
+            left += 1
+            
+            #add current character
+        char_set.add(s[right])
+            
+            # update the maxm length 
+        max_length = max(max_length, right - left + 1)
+            
+    return max_length
+    
+    
+s = "abcabcbb"
+result = len_of_longest_substring(s)
+
+print("Length of longest substring without repeating characters:", result)
