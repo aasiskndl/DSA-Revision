d35539f40b48ea7954c7cb4b9eaaed2d2e743e29
aasiskndl <aashishkandel90@gmail.com>
2026-01-28 22:13:22 +0545

    Studied the statement of problem (Capacity to ship packages in D days

---
+++ core_dsa/binary_search_on_answer/capacity_to_ship_packages.py
@@ -0,0 +1,13 @@
+'''
+Docstring for core_dsa.binary_search_on_answer.capacity_to_ship_packages
+You are given weights of packages on a conveyor belt
+You must ship them in order within D days
+
+Each day you load packages until the ship reaches its weight capacity.
+Return the minimum ship capacity needed.
+
+We are searching for the minimum capacity such that we can ship all packages in <= D days
+'''
+
+
+
