eaf9d015950501352fba7815512221b0b4a20727
aasiskndl <aashishkandel90@gmail.com>
2026-01-29 22:22:48 +0545

    Learnt about the problem of backtracking permutations

---
+++ dsa/backtracking/permutations.py
@@ -0,0 +1,10 @@
+'''
+Docstring for dsa.backtracking.permutations
+Backtracking for permutation is a systemaic, recursive algorithm that generates all possible ordering of a set of element by building candidates incrementally.
+It explores potential sequences, and upon reaching a full permutaion or invalid state,
+it undoes the last choicee (backtracks) to try alternative options, creating a tree of possibilities.
+
+goal is to generate all possible ordered arrangements (permutation) of a given set of distinct elements.
+
+'''
+
