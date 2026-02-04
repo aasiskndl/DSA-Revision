a01d4354b11c997089f4f244a0d61044c0f24419
aasiskndl <aashishkandel90@gmail.com>
2026-02-04 23:39:58 +0545

    Learnt about the two subset problem

---
+++ dsa/core_dsa/two_pointers.py
@@ -0,0 +1,10 @@
+'''
+Docstring for dsa.core_dsa.two_pointers
+Two-pointers technique is a DSA pattern that uses two indices to traverse linear data structures (arrays, strings or linked lists)
+simultaneously, often reducing O(n^2) complexity to O(n). 
+It is primarily used for sorting/searching pairs, finding or reversing elements by moving pointers inward, outward or in the same direction.
+
+Working: Instead of nested loops, two pointers move from start and end or both from start, adjusting based on conditions.
+'''
+
+
