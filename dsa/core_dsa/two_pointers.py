0fb8338d86ceb628ad862f17f4e88efaedde7ed6
aasiskndl <aashishkandel90@gmail.com>
2026-02-04 23:44:24 +0545

    Implemented the example of two sum problem

--- dsa/core_dsa/two_pointers.py
+++ dsa/core_dsa/two_pointers.py
@@ -7,4 +7,24 @@ It is primarily used for sorting/searching pairs, finding or reversing elements 
 Working: Instead of nested loops, two pointers move from start and end or both from start, adjusting based on conditions.
 '''
 
+# problem: find if there is a pair with sum = X.
 
+def pair_with_sum(arr, target):
+    #pointers
+    left = 0
+    right = len(arr) - 1
+    
+    while left < right:
+        current_sum = arr[left] + arr[right]
+        
+        if current_sum == target:
+            return True
+        elif current_sum < target:
+            left = left + 1
+        else:
+            right -= 1
+    return False
+
+arr = [1,2,3,4,5,6]
+target=6
+print(pair_with_sum(arr, target))
