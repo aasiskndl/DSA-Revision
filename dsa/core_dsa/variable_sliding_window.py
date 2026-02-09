aa48f5d11c2c01396ab4385902fca626715e5382
Aashish Kandel <aashishkandel90@gmail.com>
2026-02-09 12:37:10 +0545

    Implemented the example of variable slidig window problem

--- dsa/core_dsa/variable_sliding_window.py
+++ dsa/core_dsa/variable_sliding_window.py
@@ -10,4 +10,25 @@ TIme complexity: it optimized O(n^2) problem to O(n) by expanding the right poin
 
 '''
 
+def length_of_longest_substring(s):
+    char_set = set()
+    left = 0
+    max_length = 0 
+    
+    for right in range(len(s)):
+        
+        # if duplicate, shrink window
+        while s[right] in char_set:
+            char_set.remove(s[left])
+            left += 1
+            
+        #add current character
+        char_set.add(s[right])
+        
+        # update max window size
+        max_length = max(max_length, right - left + 1)
+        
+    return max_length
+
+print(length_of_longest_substring("pwwdeew"))
 
