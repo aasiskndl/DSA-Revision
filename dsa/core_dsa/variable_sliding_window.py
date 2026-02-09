c5bb31065ff1cef647fd013600a5263c489682b3
Aashish Kandel <aashishkandel90@gmail.com>
2026-02-09 12:32:04 +0545

    Learnt about variable sliding window problem

---
+++ dsa/core_dsa/variable_sliding_window.py
@@ -0,0 +1,13 @@
+'''
+Docstring for dsa.core_dsa.variable_sliding_window
+Variable sliding window is the problem used to find an optimal contigious subarray or substring by dynamically adjusting the window size 
+based on specific constraints, rather than using a fixed size. 
+
+left pointer -> at the start of window
+right pointer -> end of window
+
+TIme complexity: it optimized O(n^2) problem to O(n) by expanding the right pointer to include elements and shrinking the left pointer when conditions are violated
+
+'''
+
+
