c4525bfa6de0ec14ffa1cf41841cea72bb6c9e23
Aashish Kandel <aashishkandel90@gmail.com>
2026-02-18 22:20:31 +0545

    learnt about longest increasing sequence

---
+++ dynamic_programming/longest_increasing_sequence.py
@@ -0,0 +1,19 @@
+# finds the length of the longest strictly increasing subseuence from an array
+
+def lis(nums):
+    
+    if not nums:
+        return 0 
+    
+    n = len(nums)
+    
+    dp = [1] * n
+    
+    for i in range(n):
+        for j in range(i):
+            if nums[j] < nums[i]:
+                dp[i] = max(dp[i], dp[j] + 1)
+                
+    return max(dp)
+
+print(lis([10,9,2,5,3,7,101,18])) 
